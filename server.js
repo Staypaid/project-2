e0d37e2ba308083660c9d80bca900f5290add269
Devin Caraza <dcaraza@gmail.com>
2019-06-26 20:30:53 -0400

    server file

--- server.js
+++ server.js
@@ -1,47 +1,40 @@
-require("dotenv").config();
 var express = require("express");
+var path = require("path");
 var exphbs = require("express-handlebars");
-
+var favicon = require("serve-favicon");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var db = require("./models");
 
+// Initialize app.
 var app = express();
-var PORT = process.env.PORT || 8080;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(path.join(__dirname, "public")));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+var PORT = process.env.PORT || 3000;
+
+// Set handlebars as view engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Favicon
+app.use(favicon(path.join(__dirname, "public/assets/images", "favicon.png")));
 
-var syncOptions = { force: false };
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// Method override.
+app.use(methodOverride("_method"));
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+// Import routes and give the server access to them.
+var routes = require("./controllers/movie_controller.js");
+app.use("/", routes);
+
+// Start listening.
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+    console.log("listening on port " + PORT);
   });
 });
-
-module.exports = app;
